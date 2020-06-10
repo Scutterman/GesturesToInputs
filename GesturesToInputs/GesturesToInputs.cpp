cf2aed8f62e75ef64b6735d0f99d689087fc0494
tom <2962703+Scutterman@users.noreply.github.com>
2020-06-10 12:18:27 +0100

    Convert main loop to use new trackers

--- GesturesToInputs/GesturesToInputs.cpp
+++ GesturesToInputs/GesturesToInputs.cpp
@@ -6,7 +6,6 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-#include "ControlWindow.h"
 #include "Gesture.h"
 #include "PerformanceTimer.h"
 #include "Tracker.h"
@@ -18,36 +17,32 @@ using namespace GesturesToInputs;
 int main(int argc, char** argv)
 {
     try {
-        ControlWindow redUpperControls = ControlWindow("Red U Controls", 169, 179, 104, 255, 151, 255);
-        ControlWindow redLowerControls = ControlWindow("Red L Controls", 0, 10, 113, 255, 58, 255);
-        ControlWindow greenControls = ControlWindow("Green Controls", 80, 95, 111, 255, 110, 255);
-        
         Webcam cam = Webcam();
         if (!cam.initialised) {
             return 0;
         }
         cam.mirroredCapture = true;
-    
-        Tracker redTracker = Tracker(cv::Scalar(0, 0, 255), cam.next().source.size());
-        Tracker greenTracker = Tracker(cv::Scalar(0, 255, 0), cam.next().source.size());
+
+        Tracker trackers[] = {
+            Tracker("Red", std::list<TrackerValues>{
+                TrackerValues(0, 10, 113, 255, 58, 255),
+                TrackerValues(169, 179, 104, 255, 151, 255),
+            }, cv::Scalar(0, 0, 255), cam.next().source.size()),
+            Tracker("Green", std::list<TrackerValues> { TrackerValues(80, 95, 111, 255, 110, 255) }, cv::Scalar(0, 255, 0), cam.next().source.size())
+        };
 
         Gesture gesture;
         PerformanceTimer perf;
+        
         for (;;) {
             perf.Start();
             WebcamFrame frame = cam.next();
-            
-            cv::Mat redThreshold = frame.isolateColour(redUpperControls.lowScalar(), redUpperControls.highScalar()) + frame.isolateColour(redLowerControls.lowScalar(), redLowerControls.highScalar());
-            redTracker.track(redThreshold);
-            
-            cv::Mat greenThreshold = frame.isolateColour(greenControls.lowScalar(), greenControls.highScalar());
-            greenTracker.track(greenThreshold);
+            for (auto& tracker : trackers) { tracker.track(frame.source); }
 
-            gesture.calculateInstructionsWithUnknownOrder(redTracker, greenTracker);
+            gesture.calculateInstructionsWithUnknownOrder(trackers[0], trackers[1]);
 
             frame.drawGrid();
-            //imshow("Threshold", redThreshold + greenThreshold); // Show our image inside it.
-            cv::imshow("Original", frame.source + redTracker.lines + greenTracker.lines);
+            cv::imshow("Original", frame.source + trackers[0].lines + trackers[1].lines);
             perf.End();
             
             if (cv::waitKey(10) == 27) {
