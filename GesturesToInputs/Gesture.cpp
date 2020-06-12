d6ec34f883f24de1ebfb73057226fae026a0fb1d
tom <2962703+Scutterman@users.noreply.github.com>
2020-06-12 12:17:26 +0100

    Remove previous gesture processing implementation

--- GesturesToInputs/Gesture.cpp
+++ GesturesToInputs/Gesture.cpp
@@ -19,17 +19,6 @@ namespace GesturesToInputs {
     }
     
     void Gesture::calculateInstructions(std::map<std::string, Tracker> trackers) {
-        /*std::map<std::string, Tracker>::iterator it = trackers.begin();
-        auto tracker1 = *it;
-        std::advance(it, 1);
-        auto tracker2 = *it;
-        if (tracker1.second.getXPosition() < tracker2.second.getXPosition()) {
-            calculateInstructions(tracker1.second, tracker2.second);
-        }
-        else {
-            calculateInstructions(tracker2.second, tracker1.second);
-        }*/
-        
         text.setTo(0);
         textLine = 40;
 
@@ -77,71 +66,11 @@ namespace GesturesToInputs {
         ip.ki.dwFlags = KEYEVENTF_SCANCODE;
         ip.ki.wScan = dikKeyCode;
         SendInput(1, &ip, sizeof(INPUT));
-        /*if (pressed[dikKeyCode] == false) {
-            pressed[dikKeyCode] = true;
-        }*/
     }
     
     void Gesture::cancelInput(int dikKeyCode) {
         ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
         ip.ki.wScan = dikKeyCode;
         SendInput(1, &ip, sizeof(INPUT));
-        /*if (pressed[dikKeyCode] == true) {
-            pressed[dikKeyCode] = false;
-        }*/
     }
-    
-    /*void Gesture::calculateInstructions(Tracker left, Tracker right) {
-        text.setTo(0);
-        textLine = 40;
-
-        if (left.getHorizontalPosition() == HORIZONTAL_POSITION::CENTRE && right.getHorizontalPosition() == HORIZONTAL_POSITION::CENTRE) {
-            log("FAST");
-            sendInput(DIK_F);
-            Sleep(100);
-            cancelInput(DIK_F);
-        }
-
-        if (left.getHorizontalPosition() == HORIZONTAL_POSITION::FAR_LEFT && right.getHorizontalPosition() == HORIZONTAL_POSITION::FAR_RIGHT) {
-            log("SLOW");
-            sendInput(DIK_SPACE);
-        }
-        else {
-            cancelInput(DIK_SPACE);
-        }
-
-        if (left.getVerticalPosition() < right.getVerticalPosition()) {
-            log("TILT LEFT");
-            sendInput(DIK_A);
-        }
-        else {
-            cancelInput(DIK_A);
-        }
-
-        if (right.getVerticalPosition() < left.getVerticalPosition()) {
-            log("TILT RIGHT");
-            sendInput(DIK_D);
-        }
-        else {
-            cancelInput(DIK_D);
-        }
-
-        if (left.getVerticalPosition() == VERTICAL_POSITION::TOP && right.getVerticalPosition() == VERTICAL_POSITION::TOP) {
-            log("FLY UP");
-            sendInput(DIK_W);
-        }
-        else {
-            cancelInput(DIK_W);
-        }
-
-        if (left.getVerticalPosition() == VERTICAL_POSITION::BOTTOM && right.getVerticalPosition() == VERTICAL_POSITION::BOTTOM) {
-            log("FLY DOWN");
-            sendInput(DIK_S);
-        }
-        else {
-            cancelInput(DIK_S);
-        }
-
-        cv::imshow("Text", text);
-    }*/
 }
