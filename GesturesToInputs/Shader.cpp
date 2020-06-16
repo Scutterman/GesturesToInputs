dcf6ad7776a74a87942b7ee909c7036c69eecd90
tom <2962703+Scutterman@users.noreply.github.com>
2020-06-16 13:12:31 +0100

    Add class for loading shaders from file

---
+++ GesturesToInputs/Shader.cpp
@@ -0,0 +1,66 @@
+#include "Shader.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string Shader::getShaderContents(std::string shaderPath)
+{
+    std::ifstream shaderFile;
+    shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    shaderFile.open(shaderPath);
+    std::stringstream shaderStream;
+    shaderStream << shaderFile.rdbuf();
+    shaderFile.close();
+    return shaderStream.str();
+}
+
+bool Shader::addShader(int shaderType, std::string shaderPath)
+{
+    GLuint shaderHandle;
+    shaderHandle = glCreateShader(shaderType);
+    auto contents = getShaderContents(shaderPath);
+    auto contentsPointer = contents.c_str();
+    glShaderSource(shaderHandle, 1, &contentsPointer, NULL);
+    glCompileShader(shaderHandle);
+    
+    GLint success;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char info[512];
+        glGetShaderInfoLog(shaderHandle, 512, NULL, info);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info << std::endl;
+        return false;
+    };
+
+    shaderHandles.push_back(shaderHandle);
+    return true;
+}
+
+bool Shader::compile()
+{
+    programHandle = glCreateProgram();
+    for (auto& shaderHandle : shaderHandles) { glAttachShader(programHandle, shaderHandle); }
+    glLinkProgram(programHandle);
+
+    GLint success;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &success);
+    if (!success) {
+        char info[512];
+        glGetProgramInfoLog(programHandle, 512, NULL, info);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info << std::endl;
+        return false;
+    }
+
+    for (auto& shaderHandle : shaderHandles) { glDeleteShader(shaderHandle); }
+    return true;
+}
+
+void Shader::use() {
+    glUseProgram(programHandle);
+}
+
+int Shader::attributeLocation(const GLchar* name)
+{
+    return glGetAttribLocation(programHandle, name);
+}
---
+++ GesturesToInputs/Shader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <list>
+#include "glad/glad.h"
+#include <string>
+
+class Shader
+{
+private:
+    std::list<GLuint> shaderHandles;
+    GLuint programHandle;
+    std::string getShaderContents(std::string shaderPath);
+public:
+    bool addShader(int shaderType, std::string pathName);
+    bool compile();
+    void use();
+    int attributeLocation(const GLchar* name);
+};
