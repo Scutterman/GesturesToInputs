196da7e3738998fd3af3864fee293ff19454d209
tom <2962703+Scutterman@users.noreply.github.com>
2020-06-25 01:45:52 +0100

    Finished implimenting media foundation webcam

--- GesturesToInputs/MediaFoundationWebcam.cpp
+++ GesturesToInputs/MediaFoundationWebcam.cpp
@@ -1,123 +1,17 @@
 #include "MediaFoundationWebcam.h"
 
 #include <Mfreadwrite.h>
+#include <mfidl.h>
 #include <mfapi.h>
 #include <mferror.h>
 #include <Shlwapi.h>
+#include <iostream>
 
 // TODO:: for more functionality see:
 // https://docs.microsoft.com/en-us/windows/win32/medfound/audio-video-capture-in-media-foundation
 // https://docs.microsoft.com/en-us/windows/win32/medfound/processing-media-data-with-the-source-reader
+// https://elcharolin.wordpress.com/2017/08/28/webcam-capture-with-the-media-foundation-sdk/
 namespace GesturesToInputs {
-    MediaFoundationWebcam::MediaFoundationWebcam(): device(NULL)
-    {
-        InitializeCriticalSection(&critsec);
-        CreateVideoCaptureDevice(&device);
-        InitialiseReader(&reader);
-    }
-
-    MediaFoundationWebcam::~MediaFoundationWebcam()
-    {
-        SafeRelease(&reader);
-        Release();
-        device->Shutdown();
-        device->Release();
-        device = NULL;
-    }
-    
-    HRESULT MediaFoundationWebcam::InitialiseReader(IMFSourceReader** ppReader)
-    {
-        HRESULT hr = S_OK;
-        IMFAttributes* pAttributes = NULL;
-
-        hr = MFCreateAttributes(&pAttributes, 1);
-        if (FAILED(hr))
-        {
-            goto done;
-        }
-
-        hr = pAttributes->SetUnknown(MF_SOURCE_READER_ASYNC_CALLBACK, this);
-        if (FAILED(hr))
-        {
-            goto done;
-        }
-
-        hr = MFCreateSourceReaderFromMediaSource(device, pAttributes, ppReader);
-
-    done:
-        SafeRelease(&pAttributes);
-        return hr;
-    }
-
-    // IMFSourceReaderCallback methods
-    STDMETHODIMP MediaFoundationWebcam::OnReadSample(HRESULT hrStatus, DWORD dwStreamIndex, DWORD dwStreamFlags, LONGLONG llTimestamp, IMFSample* pSample)
-    {
-        EnterCriticalSection(&critsec);
-        if (SUCCEEDED(hrStatus))
-        {
-            if (pSample)
-            {
-                // Do something with the sample.
-            }
-        }
-
-        LeaveCriticalSection(&critsec);
-        return S_OK;
-    }
-
-    bool MediaFoundationWebcam::newFrameAvailable()
-    {
-        return hasNewFrame;
-    }
-
-    HRESULT CreateVideoCaptureDevice(IMFMediaSource** ppSource)
-    {
-        *ppSource = NULL;
-
-        UINT32 count = 0;
-
-        IMFAttributes* pConfig = NULL;
-        IMFActivate** ppDevices = NULL;
-
-        // Create an attribute store to hold the search criteria.
-        HRESULT hr = MFCreateAttributes(&pConfig, 1);
-
-        // Request video capture devices.
-        if (SUCCEEDED(hr))
-        {
-            hr = pConfig->SetGUID(
-                MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-                MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
-            );
-        }
-
-        // Enumerate the devices,
-        if (SUCCEEDED(hr))
-        {
-            hr = MFEnumDeviceSources(pConfig, &ppDevices, &count);
-        }
-
-        // Create a media source for the first device in the list.
-        if (SUCCEEDED(hr))
-        {
-            if (count > 0)
-            {
-                hr = ppDevices[0]->ActivateObject(IID_PPV_ARGS(ppSource));
-            }
-            else
-            {
-                hr = MF_E_NOT_FOUND;
-            }
-        }
-
-        for (DWORD i = 0; i < count; i++)
-        {
-            ppDevices[i]->Release();
-        }
-        CoTaskMemFree(ppDevices);
-        return hr;
-    }
-
     template <class T> void SafeRelease(T** ppT)
     {
         if (*ppT)
@@ -126,7 +20,291 @@ namespace GesturesToInputs {
             *ppT = NULL;
         }
     }
+
+    MediaFoundationWebcam::MediaFoundationWebcam()
+    {
+        InitializeCriticalSection(&critsec);
+        refCount = 1;
+        width = 0;
+        height = 0;
+        reader = NULL;
+        buffer1 = NULL;
+        buffer2 = NULL;
+    }
+
+    MediaFoundationWebcam::~MediaFoundationWebcam()
+    {
+        EnterCriticalSection(&critsec);
+        SafeRelease(&reader);
+            
+        if (buffer1) {
+            delete buffer1;
+            buffer1 = NULL;
+        }
+        
+        if (buffer2) {
+            delete buffer2;
+            buffer2 = NULL;
+        }
+
+        LeaveCriticalSection(&critsec);
+        DeleteCriticalSection(&critsec);
+    }
+
+    HRESULT MediaFoundationWebcam::CreateVideoCaptureDevice()
+    {
+        HRESULT hr = S_OK;
+
+        //this is important!!
+        hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+
+        UINT32 count = 0;
+        IMFAttributes* attributes = NULL;
+        IMFActivate** devices = NULL;
+
+        // Create an attribute store to hold the search criteria.
+        hr = MFCreateAttributes(&attributes, 1);
+
+        // Request video capture devices.
+        if (SUCCEEDED(hr))
+        {
+            hr = attributes->SetGUID(
+                MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
+                MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
+            );
+        }
+
+        // Enumerate the devices,
+        if (SUCCEEDED(hr))
+        {
+            hr = MFEnumDeviceSources(attributes, &devices, &count);
+        }
+
+        // Create a media source for the first device in the list.
+        if (SUCCEEDED(hr))
+        {
+            if (count > 0)
+            {
+                hr = InitialiseReader(devices[0]);
+                if (SUCCEEDED(hr))
+                {
+                    bytesPerPixel = abs(stride) / width;
+                    buffer1 = new unsigned char[width * height * bytesPerPixel];
+                    buffer2 = new unsigned char[width * height * bytesPerPixel];
+                }
+                //hr = ->ActivateObject(IID_PPV_ARGS(ppSource));
+            }
+        }
+
+        SafeRelease(&attributes);
+
+        for (DWORD i = 0; i < count; i++) {
+            SafeRelease(&devices[i]);
+        }
+
+        CoTaskMemFree(devices);
+        return hr;
+    }
+
+    HRESULT MediaFoundationWebcam::InitialiseReader(IMFActivate* device)
+    {
+        HRESULT hr = S_OK;
+
+        IMFMediaSource* source = NULL;
+        IMFAttributes* attributes = NULL;
+        IMFMediaType* mediaType = NULL;
+
+        EnterCriticalSection(&critsec);
+        hr = device->ActivateObject(__uuidof(IMFMediaSource), (void**)&source);
+        if (FAILED(hr))
+        {
+            goto done;
+        }
+        
+        hr = MFCreateAttributes(&attributes, 2);
+        if (FAILED(hr))
+        {
+            goto done;
+        }
+
+        hr = attributes->SetUINT32(MF_READWRITE_DISABLE_CONVERTERS, TRUE);
+        if (FAILED(hr))
+        {
+            goto done;
+        }
+
+        hr = attributes->SetUnknown(MF_SOURCE_READER_ASYNC_CALLBACK, this);
+        if (FAILED(hr))
+        {
+            goto done;
+        }
+
+        hr = MFCreateSourceReaderFromMediaSource(source, attributes, &reader);
+        if (FAILED(hr))
+        {
+            goto done;
+        }
+
+        for (DWORD i = 0; ; i++)
+        {
+            hr = reader->GetNativeMediaType((DWORD)MF_SOURCE_READER_FIRST_VIDEO_STREAM, i, &mediaType);
+            if (FAILED(hr)) { break; }
+
+            hr = IsMediaTypeSupported(mediaType);
+            if (FAILED(hr)) { break; }
+            
+            //Get width and height
+            MFGetAttributeSize(mediaType, MF_MT_FRAME_SIZE, &width, &height);
+            if (mediaType)
+            {
+                mediaType->Release(); mediaType = NULL;
+            }
+
+            if (SUCCEEDED(hr)) {
+                // Found an output type.
+                break;
+            }
+        }
+
+        if (SUCCEEDED(hr))
+        {
+            hr = reader->ReadSample((DWORD)MF_SOURCE_READER_FIRST_VIDEO_STREAM, 0, NULL, NULL, NULL, NULL);
+        }
+
+        if (FAILED(hr))
+        {
+            if (source)
+            {
+                source->Shutdown();
+            }
+        }
+        
+    done:
+        SafeRelease(&source);
+        SafeRelease(&attributes);
+        SafeRelease(&mediaType);
+        LeaveCriticalSection(&critsec);
+        return hr;
+    }
+
+    HRESULT MediaFoundationWebcam::IsMediaTypeSupported(IMFMediaType* pType)
+    {
+        HRESULT hr = S_OK;
+
+        BOOL bFound = FALSE;
+        GUID subtype = { 0 };
+
+        //Get the stride for this format so we can calculate the number of bytes per pixel
+        GetDefaultStride(pType, &stride);
+
+        if (FAILED(hr)) { return hr; }
+        hr = pType->GetGUID(MF_MT_SUBTYPE, &subtype);
+
+        if (FAILED(hr)) { return hr; }
+
+        videoFormat = subtype;
+        if (subtype == MFVideoFormat_RGB32 ||  subtype == MFVideoFormat_RGB24)
+            return S_OK;
+        else
+            return S_FALSE;
+
+        return hr;
+    }
     
+    HRESULT MediaFoundationWebcam::GetDefaultStride(IMFMediaType* type, LONG* stride)
+    {
+        LONG tempStride = 0;
+
+        // Try to get the default stride from the media type.
+        HRESULT hr = type->GetUINT32(MF_MT_DEFAULT_STRIDE, (UINT32*)&tempStride);
+        if (FAILED(hr))
+        {
+            //Setting this atribute to NULL we can obtain the default stride
+            GUID subtype = GUID_NULL;
+
+            UINT32 width = 0;
+            UINT32 height = 0;
+
+            // Obtain the subtype
+            hr = type->GetGUID(MF_MT_SUBTYPE, &subtype);
+            //obtain the width and height
+            if (SUCCEEDED(hr))
+                hr = MFGetAttributeSize(type, MF_MT_FRAME_SIZE, &width, &height);
+            //Calculate the stride based on the subtype and width
+            if (SUCCEEDED(hr))
+                hr = MFGetStrideForBitmapInfoHeader(subtype.Data1, width, &tempStride);
+            // set the attribute so it can be read
+            if (SUCCEEDED(hr))
+                (void)type->SetUINT32(MF_MT_DEFAULT_STRIDE, UINT32(tempStride));
+        }
+
+        if (SUCCEEDED(hr))
+            *stride = tempStride;
+        return hr;
+    }
+
+    STDMETHODIMP MediaFoundationWebcam::OnReadSample(HRESULT status, DWORD dwStreamIndex, DWORD dwStreamFlags, LONGLONG llTimestamp, IMFSample* sample)
+    {
+        timer.Start();
+        HRESULT hr = S_OK;
+        IMFMediaBuffer* mediaBuffer = NULL;
+
+        EnterCriticalSection(&critsec);
+        if (FAILED(status)) {
+            hr = status;
+        }
+
+        if (SUCCEEDED(hr) && sample)
+        {
+            hr = sample->GetBufferByIndex(0, &mediaBuffer);
+            
+            if (SUCCEEDED(hr))
+            {
+                unsigned char* data;
+                mediaBuffer->Lock(&data, NULL, NULL);
+                if (readFromBuffer1 == false) {
+                    CopyMemory(buffer1, data, width * height * bytesPerPixel);
+                }
+                else {
+                    CopyMemory(buffer2, data, width * height * bytesPerPixel);
+                }
+                readFromBuffer1 = !readFromBuffer1;
+                hasNewFrame = true;
+                // TODO:: Raise an event that notifies of a new frame.
+                // If there wasn't a new frame last time the program checked it will need a hint to check again.
+                framesCollected++;
+                timer.End();
+            }
+        }
+        
+        if (SUCCEEDED(hr)) {
+            hr = reader->ReadSample((DWORD)MF_SOURCE_READER_FIRST_VIDEO_STREAM, 0, NULL, NULL, NULL, NULL);
+        }
+        
+        SafeRelease(&mediaBuffer);
+        LeaveCriticalSection(&critsec);
+        return hr;
+    }
+
+    bool MediaFoundationWebcam::newFrameAvailable()
+    {
+        return hasNewFrame;
+    }
+
+    unsigned char* MediaFoundationWebcam::getData()
+    {
+        hasNewFrame = false;
+        return readFromBuffer1 ? buffer1 : buffer2;
+    }
+
+    HRESULT MediaFoundationWebcam::Close()
+    {
+        EnterCriticalSection(&critsec);
+        SafeRelease(&reader);
+        LeaveCriticalSection(&critsec);
+        return S_OK;
+    }
+
     // IUnknown methods
     STDMETHODIMP MediaFoundationWebcam::QueryInterface(REFIID iid, void** ppv)
     {
--- GesturesToInputs/MediaFoundationWebcam.h
+++ GesturesToInputs/MediaFoundationWebcam.h
@@ -1,30 +1,41 @@
 #pragma once
+#include "PerformanceTimer.h"
 #include <mfidl.h>
+#include <mfapi.h>
 #include <Mfreadwrite.h>
 
 namespace GesturesToInputs {
     class MediaFoundationWebcam : IMFSourceReaderCallback
     {
     private:
-        IMFMediaSource* device;
+        bool isDestructing = false;
         IMFSourceReader* reader;
+        unsigned int width = 0;
+        unsigned int height = 0;
+        long stride = 0;
+        int bytesPerPixel = 0;
         bool hasNewFrame = false;
 
         unsigned char* buffer1;
-        unsigned int buffer1Length;
         unsigned char* buffer2;
-        unsigned int buffer2Length;
-        unsigned char** activeBuffer;
-        unsigned int* activeBufferLength;
-
+        bool readFromBuffer1 = false;
+        
+        PerformanceTimer timer;
         CRITICAL_SECTION critsec;
-        long refCount;
+        long refCount = 0;
 
-        HRESULT InitialiseReader(IMFSourceReader** ppReader);
+        HRESULT InitialiseReader(IMFActivate* device);
+        HRESULT IsMediaTypeSupported(IMFMediaType* pType);
+        HRESULT GetDefaultStride(IMFMediaType* type, LONG* stride);
     public:
+        GUID videoFormat;
+        int framesCollected = 0;
         MediaFoundationWebcam();
         ~MediaFoundationWebcam();
+        HRESULT CreateVideoCaptureDevice();
+        HRESULT Close();
         bool newFrameAvailable();
+        unsigned char* getData();
 
         // Inherited via IMFSourceReaderCallback        
         STDMETHODIMP QueryInterface(REFIID riid, void** ppvObject) override;
