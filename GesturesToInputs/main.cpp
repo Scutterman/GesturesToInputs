7e959a9d2d97a926374bb6a6875cbdbf38e4f16a
tom <2962703+Scutterman@users.noreply.github.com>
2020-07-01 22:12:09 +0100

    Set OpenGL 4.4 as the minimum version

--- GesturesToInputs/main.cpp
+++ GesturesToInputs/main.cpp
@@ -307,7 +307,7 @@ int setup() {
     glfwWindowHint(GLFW_CONTEXT_ROBUSTNESS, GLFW_LOSE_CONTEXT_ON_RESET);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(sourceWidth, sourceHeight, "Tracked Objects", NULL, NULL);
